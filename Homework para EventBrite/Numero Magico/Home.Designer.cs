e6f7283f6dd05cfd7e785dcb94aa2e8e3e83cb83
marcos fernandez <marcos_jujuy17@hotmail.com>
2018-05-21 13:26:35 -0300

    Diseño de la interfaz y definicion de comportamiento de los componentes

--- Homework para EventBrite/Numero Magico/Home.Designer.cs
+++ Homework para EventBrite/Numero Magico/Home.Designer.cs
@@ -28,20 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.radioGroup = new System.Windows.Forms.GroupBox();
+            this.vsComputer = new System.Windows.Forms.RadioButton();
+            this.vsHuman = new System.Windows.Forms.RadioButton();
+            this.vsComputerGroup = new System.Windows.Forms.GroupBox();
+            this.vsHumanGroup = new System.Windows.Forms.GroupBox();
+            this.vsComputerLog = new System.Windows.Forms.RichTextBox();
+            this.NumeroTxtBox = new System.Windows.Forms.TextBox();
+            this.preguntaBtn = new System.Windows.Forms.Button();
+            this.vsHumanLog = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioGroup.SuspendLayout();
+            this.vsComputerGroup.SuspendLayout();
+            this.vsHumanGroup.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // radioGroup
+            // 
+            this.radioGroup.Controls.Add(this.vsHumanGroup);
+            this.radioGroup.Controls.Add(this.vsComputerGroup);
+            this.radioGroup.Controls.Add(this.vsHuman);
+            this.radioGroup.Controls.Add(this.vsComputer);
+            this.radioGroup.Location = new System.Drawing.Point(12, 12);
+            this.radioGroup.Name = "radioGroup";
+            this.radioGroup.Size = new System.Drawing.Size(932, 528);
+            this.radioGroup.TabIndex = 0;
+            this.radioGroup.TabStop = false;
+            this.radioGroup.Text = "Modo de Juego";
+            // 
+            // vsComputer
+            // 
+            this.vsComputer.AutoSize = true;
+            this.vsComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vsComputer.Location = new System.Drawing.Point(129, 42);
+            this.vsComputer.Name = "vsComputer";
+            this.vsComputer.Size = new System.Drawing.Size(194, 29);
+            this.vsComputer.TabIndex = 0;
+            this.vsComputer.TabStop = true;
+            this.vsComputer.Text = "Adivina mi Número";
+            this.vsComputer.UseVisualStyleBackColor = true;
+            this.vsComputer.CheckedChanged += new System.EventHandler(this.vsComputer_CheckedChanged);
+            // 
+            // vsHuman
+            // 
+            this.vsHuman.AutoSize = true;
+            this.vsHuman.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vsHuman.Location = new System.Drawing.Point(601, 42);
+            this.vsHuman.Name = "vsHuman";
+            this.vsHuman.Size = new System.Drawing.Size(207, 29);
+            this.vsHuman.TabIndex = 1;
+            this.vsHuman.TabStop = true;
+            this.vsHuman.Text = "Adivinaré tu Numero";
+            this.vsHuman.UseVisualStyleBackColor = true;
+            // 
+            // vsComputerGroup
+            // 
+            this.vsComputerGroup.Controls.Add(this.preguntaBtn);
+            this.vsComputerGroup.Controls.Add(this.NumeroTxtBox);
+            this.vsComputerGroup.Controls.Add(this.vsComputerLog);
+            this.vsComputerGroup.Location = new System.Drawing.Point(6, 77);
+            this.vsComputerGroup.Name = "vsComputerGroup";
+            this.vsComputerGroup.Size = new System.Drawing.Size(445, 445);
+            this.vsComputerGroup.TabIndex = 2;
+            this.vsComputerGroup.TabStop = false;
+            // 
+            // vsHumanGroup
+            // 
+            this.vsHumanGroup.Controls.Add(this.button4);
+            this.vsHumanGroup.Controls.Add(this.button3);
+            this.vsHumanGroup.Controls.Add(this.button2);
+            this.vsHumanGroup.Controls.Add(this.vsHumanLog);
+            this.vsHumanGroup.Location = new System.Drawing.Point(481, 77);
+            this.vsHumanGroup.Name = "vsHumanGroup";
+            this.vsHumanGroup.Size = new System.Drawing.Size(445, 445);
+            this.vsHumanGroup.TabIndex = 3;
+            this.vsHumanGroup.TabStop = false;
+            // 
+            // vsComputerLog
+            // 
+            this.vsComputerLog.Location = new System.Drawing.Point(7, 20);
+            this.vsComputerLog.Name = "vsComputerLog";
+            this.vsComputerLog.ReadOnly = true;
+            this.vsComputerLog.Size = new System.Drawing.Size(432, 301);
+            this.vsComputerLog.TabIndex = 0;
+            this.vsComputerLog.Text = "";
+            // 
+            // NumeroTxtBox
+            // 
+            this.NumeroTxtBox.Location = new System.Drawing.Point(7, 328);
+            this.NumeroTxtBox.Name = "NumeroTxtBox";
+            this.NumeroTxtBox.Size = new System.Drawing.Size(289, 20);
+            this.NumeroTxtBox.TabIndex = 1;
+            this.NumeroTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumeroTxtBox_KeyPress);
+            // 
+            // preguntaBtn
+            // 
+            this.preguntaBtn.Location = new System.Drawing.Point(303, 328);
+            this.preguntaBtn.Name = "preguntaBtn";
+            this.preguntaBtn.Size = new System.Drawing.Size(136, 23);
+            this.preguntaBtn.TabIndex = 2;
+            this.preguntaBtn.Text = "Es tu Número?";
+            this.preguntaBtn.UseVisualStyleBackColor = true;
+            this.preguntaBtn.Click += new System.EventHandler(this.preguntaBtn_Click);
+            // 
+            // vsHumanLog
+            // 
+            this.vsHumanLog.Location = new System.Drawing.Point(7, 20);
+            this.vsHumanLog.Name = "vsHumanLog";
+            this.vsHumanLog.ReadOnly = true;
+            this.vsHumanLog.Size = new System.Drawing.Size(432, 301);
+            this.vsHumanLog.TabIndex = 0;
+            this.vsHumanLog.Text = "";
+            this.vsHumanLog.TextChanged += new System.EventHandler(this.vsHumanLog_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(7, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Menor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(329, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Mayor";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(167, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Correcto!";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(956, 552);
+            this.Controls.Add(this.radioGroup);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Home";
-            this.Text = "Numero Magico";
+            this.Text = "Numero Mágico";
+            this.radioGroup.ResumeLayout(false);
+            this.radioGroup.PerformLayout();
+            this.vsComputerGroup.ResumeLayout(false);
+            this.vsComputerGroup.PerformLayout();
+            this.vsHumanGroup.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox radioGroup;
+        private System.Windows.Forms.GroupBox vsHumanGroup;
+        private System.Windows.Forms.GroupBox vsComputerGroup;
+        private System.Windows.Forms.RadioButton vsHuman;
+        private System.Windows.Forms.RadioButton vsComputer;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox vsHumanLog;
+        private System.Windows.Forms.Button preguntaBtn;
+        private System.Windows.Forms.TextBox NumeroTxtBox;
+        private System.Windows.Forms.RichTextBox vsComputerLog;
     }
 }
 
--- Homework para EventBrite/Numero Magico/Home.cs
+++ Homework para EventBrite/Numero Magico/Home.cs
@@ -16,5 +16,52 @@ namespace Numero_Magico
         {
             InitializeComponent();
         }
+
+        private void Cleaner()
+        {
+            vsComputerLog.Clear();
+            vsHumanLog.Clear();
+            NumeroTxtBox.Clear();
+        }
+        private void vsComputer_CheckedChanged(object sender, EventArgs e)
+        {
+            Cleaner();
+            vsComputerGroup.Enabled = true;
+            vsHumanGroup.Enabled = false;
+            vsComputerLog.AppendText("Adivina mi número \n");
+        }
+
+        private void vsHumanLog_TextChanged(object sender, EventArgs e)
+        {
+            Cleaner();
+            vsComputerGroup.Enabled = false;
+            vsHumanGroup.Enabled = true;
+            vsHumanLog.AppendText("?\n");
+        }
+
+        private void NumeroTxtBox_KeyPress(object sender, KeyPressEventArgs e) //Controla la entrada de caracteres en TxtBox. 
+                                                                               //Acepta Numeros y controles (retroceso, enter, etc.)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            } else if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+                if (e.KeyChar == Convert.ToChar(Keys.Enter)) // Presionar Enter equivale a hacer click en preguntaBtn
+                {
+                    preguntaBtn_Click(sender, e);
+                }
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void preguntaBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
